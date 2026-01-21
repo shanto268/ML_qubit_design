8f7b0eaabc2589416d138ad8e4a25d63fa46f33d
OliviaSeidel <seidelolivia2@gmail.com>
2026-01-21 01:41:23 +0000

    first retuning using keras tuner

--- model_predict_cavity_claw_RouteMeander_eigenmode/parameters.py
+++ model_predict_cavity_claw_RouteMeander_eigenmode/parameters.py
@@ -5,11 +5,11 @@ DATA_DIR = 'data'
 DATASETS_JSON = DATA_DIR + '/datasets.json'
 
 SWEEP_PARAM_NUM = False
-SWEEP_DATA_AMOUNT = True
+SWEEP_DATA_AMOUNT = False
 VISUALIZE_GRADIENTS = False
 
 KERAS_TUNER = False
-KERAS_TUNER_TRIALS = 42
+KERAS_TUNER_TRIALS = 53
 KERAS_DIR = 'keras'
 
 ENCODING_TYPE = 'one hot' # need to pass 'one hot' or 'linear' or 'Try Both'
@@ -20,7 +20,7 @@ DATA_AUGMENTATION = True
 # We use a simple fully connected network (MLP) 
 # 4 layers because deeper NNs can capture more complex patterns
 # Gradually decrease the neuron size to better capture patterns while avoiding overfitting
-NEURONS_PER_LAYER = [570,510,250,440,480]
+NEURONS_PER_LAYER = [430,550,580,270,550]#[570,510,250,440,480]
 TRAIN_DROPOUT_RATE = 0#0.05
 
 # Training hyper-parameters
@@ -28,7 +28,7 @@ TRAIN_DROPOUT_RATE = 0#0.05
 # Learning Rate gives the step size that the optimizer takes while learning, 
 # smaller step size means slower convergence but more accuracy
 # learning rate is=LR_INITIAL×(LR_DECAY_RATE)^(t/LR_DECAY_STEPS)
-LR_INITIAL = 0.000982
+LR_INITIAL = 0.0049807
 
 # Learning rate decay helps the model become refined as it gets closer to a minimum
 # The learning rate decay steps desides how many steps the learning rate will decay after
