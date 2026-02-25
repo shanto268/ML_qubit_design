524346120773f35b517dca2a4c39729ae26fc7d0
OliviaSeidel <seidelolivia2@gmail.com>
2026-02-25 04:00:43 +0000

    updated all models to have a second model to serve as an ansys surrogate. the ml_1... notebooks contain setups for these

---
+++ model_predict_cavity_claw_RouteMeander_eigenmode/parameters_surrogate.py
@@ -0,0 +1,52 @@
+# resonator/cavity claw route meander
+
+# Where the datasets are
+DATA_DIR = 'data'
+DATASETS_JSON = DATA_DIR + '/datasets.json'
+
+SWEEP_PARAM_NUM = False
+SWEEP_DATA_AMOUNT = False
+VISUALIZE_GRADIENTS = False
+
+KERAS_TUNER = True
+KERAS_TUNER_TRIALS = 1
+KERAS_DIR = 'keras'
+
+ENCODING_TYPE = 'one hot' # need to pass 'one hot' or 'linear' or 'Try Both'
+
+# Enable data augmentation/scaling, etc
+DATA_AUGMENTATION = True
+
+# We use a simple fully connected network (MLP) 
+# 4 layers because deeper NNs can capture more complex patterns
+# Gradually decrease the neuron size to better capture patterns while avoiding overfitting
+NEURONS_PER_LAYER = [430,550,580,270,550]#[570,510,250,440,480]
+TRAIN_DROPOUT_RATE = 0#0.05
+
+# Training hyper-parameters
+
+# Learning Rate gives the step size that the optimizer takes while learning, 
+# smaller step size means slower convergence but more accuracy
+# learning rate is=LR_INITIAL×(LR_DECAY_RATE)^(t/LR_DECAY_STEPS)
+LR_INITIAL = 0.0049807
+
+# Learning rate decay helps the model become refined as it gets closer to a minimum
+# The learning rate decay steps desides how many steps the learning rate will decay after
+# LR_DECAY_STEPS = 35  # 100 best for log phig1 cadence data
+
+# LR_INITIAL * LR_DECAY_RATE after each number of LR_DECAY_STEPS
+LR_DECAY_RATE = 0.99
+
+# Staircase or continuous?
+LR_STAIRCASE = False
+
+TRAIN_EARLY_STOPPING_PATIENCE = 60
+TRAIN_BATCH_SIZE = 128 # 32 default
+#TRAIN_VALIDATION_SPLIT = 0.2
+
+#TRAIN_LOSS = 'mean_squared_error'
+TRAIN_LOSS = 'mae' # mean absolute error
+#TRAIN_LOSS = 'mean_squared_logarithmic_error'
+
+EPOCHS = 400
+
