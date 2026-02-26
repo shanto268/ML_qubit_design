b1ff1e671d4fbb8ac056049d74591c99c97b2135
OliviaSeidel <seidelolivia2@gmail.com>
2026-02-26 15:12:31 +0000

    first try at ncap combined two models

--- model_predict_coupler_NCap_cap_matrix/parameters_surrogate_defined_loss.py
+++ model_predict_coupler_NCap_cap_matrix/parameters_surrogate_defined_loss.py
@@ -1,4 +1,4 @@
-# resonator/cavity claw route meander
+# coupling claw cap
 
 # Where the datasets are
 DATA_DIR = 'data'
@@ -9,7 +9,7 @@ SWEEP_DATA_AMOUNT = False
 VISUALIZE_GRADIENTS = False
 
 KERAS_TUNER = True
-KERAS_TUNER_TRIALS = 222
+KERAS_TUNER_TRIALS = 303
 KERAS_DIR = 'keras'
 
 ENCODING_TYPE = 'one hot' # need to pass 'one hot' or 'linear' or 'Try Both'
@@ -20,15 +20,15 @@ DATA_AUGMENTATION = True
 # We use a simple fully connected network (MLP) 
 # 4 layers because deeper NNs can capture more complex patterns
 # Gradually decrease the neuron size to better capture patterns while avoiding overfitting
-NEURONS_PER_LAYER = [430,550,580,270,550]#[570,510,250,440,480]
-TRAIN_DROPOUT_RATE = 0#0.05
+NEURONS_PER_LAYER = [264,464,364,364,864]
+TRAIN_DROPOUT_RATE = 0.06
 
 # Training hyper-parameters
 
 # Learning Rate gives the step size that the optimizer takes while learning, 
 # smaller step size means slower convergence but more accuracy
 # learning rate is=LR_INITIAL×(LR_DECAY_RATE)^(t/LR_DECAY_STEPS)
-LR_INITIAL = 0.0049807
+LR_INITIAL = 0.00092875
 
 # Learning rate decay helps the model become refined as it gets closer to a minimum
 # The learning rate decay steps desides how many steps the learning rate will decay after
@@ -41,12 +41,12 @@ LR_DECAY_RATE = 0.99
 LR_STAIRCASE = False
 
 TRAIN_EARLY_STOPPING_PATIENCE = 60
-TRAIN_BATCH_SIZE = 128 # 32 default
+TRAIN_BATCH_SIZE = 90 #128 # 32 default
 #TRAIN_VALIDATION_SPLIT = 0.2
 
 #TRAIN_LOSS = 'mean_squared_error'
 TRAIN_LOSS = 'mae' # mean absolute error
 #TRAIN_LOSS = 'mean_squared_logarithmic_error'
 
-EPOCHS = 400
+
 
